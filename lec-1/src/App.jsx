a925d58b26a4af3c6001d34ba280647ebfc78f71
haroon nadir <haroonnadir143@gmail.com>
2024-10-26 04:04:35 +0500

    first commit

---
+++ lec-1/src/App.jsx
@@ -0,0 +1,4 @@
+export const App = () => {
+  return <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique qui voluptate, quo quod maxime minus. Eligendi dolorem sapiente magni non quos quas expedita veniam labore est quo, velit delectus explicabo!</p>;
+};
+
